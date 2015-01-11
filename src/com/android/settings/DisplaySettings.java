ae61a28c997985bef651da65d6c68fb526e72386
dankoman <dankoman30@gmail.com>
2015-01-11 05:56:00 -0600

    [2/2] Settings: Doze (ambient display) timeout
    Patchset: put the preference in more sensible location.
    Patchset: remove preference for devices that don't support
            doze display state.
    Patchset: turn off doze timeout slider if doze is disabled
    Patchset: move doze options to own preference category
    Patchset: merge with http://gerrit.dirtyunicorns.com/#/c/9286/2
    
    Change-Id: I366a3a00bb48c20fa2d37f0b22fdbfeb3d004533
    
    Conflicts:
    
    	res/values/rr_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -58,6 +58,7 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -106,7 +107,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
+<<<<<<< HEAD
     private ListPreference mLcdDensityPreference;
+=======
+    private static final String KEY_DOZE_TIMEOUT = "doze_timeout";
+
+>>>>>>> a11ba38... [2/2] Settings: Doze (ambient display) timeout
     private FontDialogPreference mFontSizePref;
     private PreferenceScreen mDisplayRotationPreference;
     private PreferenceScreen mScreenColorSettings;
@@ -124,6 +130,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mAdaptiveBacklight;
     private SwitchPreference mSunlightEnhancement;
     private SwitchPreference mColorEnhancement;
+
+    private SlimSeekBarPreference mDozeTimeout;
 
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
@@ -229,8 +237,19 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (isDozeAvailable(activity)) {
             mDozePreference = (SwitchPreference) findPreference(KEY_DOZE);
             mDozePreference.setOnPreferenceChangeListener(this);
+            // Doze timeout
+            mDozeTimeout = (SlimSeekBarPreference) findPreference(KEY_DOZE_TIMEOUT);
+            if (mDozeTimeout != null) {
+                mDozeTimeout.setDefault(3000);
+                mDozeTimeout.isMilliseconds(true);
+                mDozeTimeout.setInterval(1);
+                mDozeTimeout.minimumValue(100);
+                mDozeTimeout.multiplyValue(100);
+                mDozeTimeout.setOnPreferenceChangeListener(this);
+            }
         } else {
             removePreference(KEY_DOZE);
+            removePreference(KEY_DOZE_TIMEOUT);
         }
 
         mTapToWake = (SwitchPreference) findPreference(KEY_TAP_TO_WAKE);
@@ -409,6 +428,15 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     @Override
     public void onResume() {
         super.onResume();
+
+        // Doze timeout
+        if (mDozeTimeout != null) {
+            final int statusDozeTimeout = Settings.PAC.getInt(getContentResolver(),
+                    Settings.PAC.DOZE_TIMEOUT, 3000);
+            // minimum 100 is 1 interval of the 100 multiplier
+            mDozeTimeout.setInitValue((statusDozeTimeout / 100) - 1);
+        }
+
         updateDisplayRotationPreferenceDescription();
         if (mAdaptiveBacklight != null) {
             mAdaptiveBacklight.setChecked(AdaptiveBacklight.isEnabled());
@@ -620,6 +648,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (preference == mDozePreference) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), DOZE_ENABLED, value ? 1 : 0);
+        }
+        if (preference == mDozeTimeout) {
+            int dozeTimeout = Integer.valueOf((String) objValue);
+            Settings.PAC.putInt(getContentResolver(),
+                    Settings.PAC.DOZE_TIMEOUT, dozeTimeout);
         }
         return true;
     }
